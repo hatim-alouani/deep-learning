1d9c656b91da1f32acff12d7ae3de416c8f464d2
Hatim Alouani <haalouan@e2r7p6.1337.ma>
2025-05-23 19:59:31 +0100

    update

--- cnnMnist.py
+++ cnnMnist.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, ReLU, Dense, Flatten, Dropout, l2
-
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, ReLU, Dense, Flatten, Dropout
+from tensorflow.keras.regularizers import l2
 (x_train , y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255
@@ -26,7 +26,7 @@ model = Sequential([
     MaxPooling2D(pool_size=2, strides=2),
 
     Flatten(),
-    Dense(units=128, activation='relu', kernel_regularizer=l2(0.001)),
+    Dense(units=128, activation='relu', kernel_regularizer=l2()),
     Dropout(0.3),
     Dense(units=10, activation='softmax')
 ])
@@ -38,9 +38,9 @@ train_generator = ImageDataGenerator(10, 0.1, 0.1, 0,1).flow(x_train, y_train, b
 model.fit(train_generator, validation_data=(x_val, y_val), epochs=10, callbacks=[EarlyStopping()])
 
 loss_test, accuracy_test = model.evaluate(x_test, y_test)
-prediction = model.predict(x_test[0:1])
+prediction = model.predict(x_test[1:2])
 prediction = np.argmax(prediction)
 print("loss test : ", loss_test)
 print("accuracy test : ", accuracy_test)
 print("predicted result : ", prediction)
-print("True result : ", y_test[0])
+print("True result : ", y_test[1])
